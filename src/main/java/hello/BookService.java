7910cf67418437a9f9acdfa8e1c14df8af5d6981
VolodymyrShpynta <vshpynta@cogniance.com>
2017-08-09 15:36:59 +0300

    Adjusted BookService

--- src/main/java/hello/BookService.java
+++ src/main/java/hello/BookService.java
@@ -3,7 +3,9 @@ package hello;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
@@ -13,14 +15,26 @@ import org.springframework.stereotype.Service;
 public class BookService {
 
     private final BookRepository bookRepository;
+    private final CacheManager cacheManager;
     private int counter = 1;
     private boolean wasError = false;
 
     @Retryable(
             include = RecoverNeededException.class,
-            maxAttempts = 2,
+            maxAttempts = 1,
             backoff = @Backoff(delay = 100, maxDelay = 101))
     public Book find(String isbn) {
+        return findInternal(isbn);
+    }
+
+    @Recover //This method is invoked to try recover after all retry fail.
+    public Book recoverAfterException(RecoverNeededException e, String isbn) {
+        log.info("Recovering after exception. Arg is '{}'. Exception is '{}'", isbn, e.getClass());
+        cacheManager.getCache("books").evict(isbn);
+        return findInternal(isbn);
+    }
+
+    private Book findInternal(String isbn) {
         errorCheck();
         return bookRepository.getByIsbn(isbn);
     }
--- src/main/java/hello/MyRetryListener.java
+++ src/main/java/hello/MyRetryListener.java
@@ -1,33 +1,28 @@
 package hello;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.CacheManager;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.retry.RetryCallback;
 import org.springframework.retry.RetryContext;
 import org.springframework.retry.RetryListener;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class CacheCleaner implements RetryListener {
-
-    private final CacheManager cacheManager;
+@Slf4j
+public class MyRetryListener implements RetryListener {
 
     @Override
     public <T, E extends Throwable> boolean open(RetryContext context, RetryCallback<T, E> callback) {
-        System.out.println("In open");
+        log.info("In open");
         return true;
     }
 
     @Override
     public <T, E extends Throwable> void close(RetryContext context, RetryCallback<T, E> callback, Throwable throwable) {
-        System.out.println("In close");
+        log.info("In close");
     }
 
     @Override
     public <T, E extends Throwable> void onError(RetryContext context, RetryCallback<T, E> callback, Throwable throwable) {
-        System.out.println("In onError");
-        cacheManager.getCache("books").clear();
+        log.info("In onError");
     }
 }
